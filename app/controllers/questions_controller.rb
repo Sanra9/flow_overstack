253c3ed152d57a95e61ed2bbb6f3ae53d7c8a5aa
sanra9 <sanrra999@gmail.com>
2018-06-17 22:01:30 -0500

    creando answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -35,6 +35,10 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :description)
